73959bd6381c2154ad170caf25e8de6c53188aab
Joaquín Gil <joacogilp@gmail.com>
2023-08-11 18:59:22 -0300

    EventListener added to a div, and div style with css

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var buttonDiv = document.getElementById("button");
+
+    buttonDiv.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
